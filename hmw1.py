953157ec79ff5ea33bfbb1e3e9c78a0cc3306983
serhanayberkkilic <78637700+swordaybo@users.noreply.github.com>
2021-03-10 19:17:51 +0300

    Add files via upload

---
+++ hmw1.py
@@ -0,0 +1,17 @@
+
+
+ciftsayilar=[]
+teksayilar=[]
+
+
+for i in range(0,100):
+
+    if i%2==0:
+        ciftsayilar.append(i)
+    else:
+        teksayilar.append(i)
+newlist=[]
+
+
+
+print(newlist)
